24bfa030112e12f483a27dee8fa4dd868d161bb5
Basil Goldman <basil@inqwise.com>
2024-12-12 22:03:48 +0100

    return outDir

---
+++ dist/ad-error.d.ts
@@ -0,0 +1,10 @@
+export default class AdError {
+    constructor(message: any, errorCode: any, innerError: any);
+    getMessage(): any;
+    getErrorCode(): any;
+    getInnerError(): any;
+    setInnerError(innerError: any): void;
+    formatMessage(...values: any[]): this;
+    toString(): string;
+    #private;
+}
